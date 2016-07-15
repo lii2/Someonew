d2665a69f8f7a8e2f47baa02e98f056cbb94a613
Chi-Young Jeffrey Lii <lii.jeffrey3@gmail.com>
2016-07-15 14:08:32 -0400

    added profile stuff
    added profile stuff

--- app/src/main/java/com/example/yjlii/someonew/ProfileActivity.java
+++ app/src/main/java/com/example/yjlii/someonew/ProfileActivity.java
@@ -4,17 +4,36 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.Profile;
 
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Profile currentUser = Profile.getCurrentProfile();
+
         setContentView(R.layout.activity_profile);
+
+        TextView FBID=(TextView)findViewById(R.id.FBID);
+        FBID.setText(currentUser.getFirstName() + " " + currentUser.getLastName());
+
+        TextView FBname=(TextView)findViewById(R.id.FBname);
+        FBID.setText(currentUser.getId());
+
+        ImageView ProfilePic = (ImageView) findViewById(R.id.profileView);
+        ProfilePic.setImageURI(currentUser.getProfilePictureUri(500, 500));
+
+
     }
 
     public void goToMain(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
 }
