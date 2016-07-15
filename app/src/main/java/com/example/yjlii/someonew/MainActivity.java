648d9b5c61dae7b2e0eb07d844b77b73ff43c4ed
lii2 <lii.jeffrey3@gmail.com>
2016-07-14 22:28:30 -0400

    working fb button
    great

--- app/src/main/java/com/example/yjlii/someonew/MainActivity.java
+++ app/src/main/java/com/example/yjlii/someonew/MainActivity.java
@@ -17,6 +17,8 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -31,48 +33,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        //measure installs on your mobile app ads, create high value audiences for targeting, and view analytics
+
         callbackManager = CallbackManager.Factory.create();
 
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-                // Go to profile page
-                Intent intent = new Intent(this, InterestActivity.class);
-                startActivity(intent);
-            }
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // If login succeeds, the LoginResult parameter has the new AccessToken, and the most recently granted or declined permissions.
+                        // Go to profiles
+                    }
 
-            @Override
-            public void onCancel() {
-                // Do nothing
-            }
+                    @Override
+                    public void onCancel() {
+                        // Stay here
+                    }
 
-            @Override
-            public void onError(FacebookException exception) {
-                // Think about later
-            }
-        });
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // Send error code
+                    }
+                });
 
 
         setContentView(R.layout.activity_main);
     }
 
-/*    public void popup(View view){
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        if(!status) {
-
-            FragmentOne f1 = new FragmentOne();
-            fragmentTransaction.add(R.id.fragment_container, f1);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
-            status = true;
-        }
-
-    }*/
 
     public void goToInterest(View view){
         Intent intent = new Intent(this, InterestActivity.class);
