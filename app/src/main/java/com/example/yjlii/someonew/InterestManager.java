e974309c094e255f5803c72aa8969446b9d9b38a
lii2 <lii.jeffrey3@gmail.com>
2016-07-15 20:32:58 -0400

    interest bar
    changed interest bar to flexbox

---
+++ app/src/main/java/com/example/yjlii/someonew/InterestManager.java
@@ -0,0 +1,45 @@
+package com.example.yjlii.someonew;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Lii2 on 7/15/2016.
+ */
+public class InterestManager {
+
+    private String[] INTEREST_ARRAY = new String[20];
+    {
+        INTEREST_ARRAY[0] = "1";
+        INTEREST_ARRAY[0] = "2";
+        INTEREST_ARRAY[0] = "3";
+        INTEREST_ARRAY[0] = "4";
+        INTEREST_ARRAY[0] = "5";
+        INTEREST_ARRAY[0] = "6";
+        INTEREST_ARRAY[0] = "7";
+        INTEREST_ARRAY[0] = "8";
+        INTEREST_ARRAY[0] = "9";
+        INTEREST_ARRAY[0] = "10";
+        INTEREST_ARRAY[0] = "11";
+        INTEREST_ARRAY[0] = "12";
+        INTEREST_ARRAY[0] = "13";
+        INTEREST_ARRAY[0] = "14";
+        INTEREST_ARRAY[0] = "15";
+        INTEREST_ARRAY[0] = "16";
+        INTEREST_ARRAY[0] = "17";
+        INTEREST_ARRAY[0] = "18";
+        INTEREST_ARRAY[0] = "19";
+        INTEREST_ARRAY[0] = "20";
+    }
+
+    private ArrayList<String> UserInterests= new ArrayList<String>();
+
+
+
+    public InterestManager() {
+
+    }
+
+
+}
